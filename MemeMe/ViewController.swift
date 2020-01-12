8ad9a3b7df1c17cef6e414a01785a7b493654c3e
Omar Mujtaba <omar.mujtaba@terem.com.au>
2020-01-12 20:44:14 +1100

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Omar Mujtaba on 12/1/20.
+//  Copyright © 2020 AmmoLogic Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
